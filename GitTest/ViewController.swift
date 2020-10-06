2660c0ec81f172f568fa1f45242c3b367957134a
Jacob Wing <jacob.wing.680@k12.friscoisd.org>
2020-10-06 11:10:37 -0500

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Wing, Jacob T on 10/6/20.
+//  Copyright © 2020 Wing, Jacob T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
